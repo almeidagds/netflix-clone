f3d385ac83450101b14557afa75f2ccb70956ac9
almeidagds <almeidag2001@gmail.com>
2021-08-25 16:30:55 -0300

    Commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-import React from 'react'
-import tmdb from './tmdb'
+import React, { useEffect, useState } from 'react'
+import Tmdb from './Tmdb'
+import MovieRow from './components/MovieRow'
+import './App.css'
+import FeaturedMovie from './components/FeaturedMovie.js'
 
 export default () => {
+
+  const [movieList, setMovieList] = useState([])
+
+  useEffect(() => {
+    const loadAll = async () => {
+      let list = await Tmdb.getHomeList()
+      setMovieList(list)
+    }
+
+    loadAll()
+  }, [])
+
   return(
-    <div> Olá, mundo
+    <div className="page"> 
+      <FeaturedMovie />
+    
+      <section className="lists">
+        {movieList.map((item, key) => (
+          <MovieRow key={key} title={item.title} items={item.items} />
+        ))}
+      </section>
     </div>
   )
 }
