6e6727876f94765d8c89eed6f621ccfeeee524e0
almeidagds <almeidag2001@gmail.com>
2021-08-27 18:21:38 -0300

    Commit

--- src/App.js
+++ src/App.js
@@ -18,8 +18,7 @@ export default () => {
       let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1))
       let chosen = originals[0].items.results[randomChosen];
       let chosenInfo = Tmdb.getMovieInfo(chosen.id, 'tv')
-
-      console.log(chosenInfo)
+      setFeaturedData(chosenInfo)
     }
 
     loadAll()
