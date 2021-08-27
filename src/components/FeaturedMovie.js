efbba299e82023a115f767cf578d71494456ecef
almeidagds <almeidag2001@gmail.com>
2021-08-27 18:57:50 -0300

    Commit

--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -7,7 +7,15 @@ export default ({item}) => {
             backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})`
         }}>
             <div className="featured--vertical">
-                <div className="featured--horizontal"></div>
+                <div className="featured--horizontal">
+                    <div className="featured--name">{item.original_name}</div>
+                    <div className="featured--info">
+                        <div className="featured--points">{item.vote_average} pontos</div>
+                        <div className="featured--year">2099</div>
+                        <div className="featured--seasons">{}</div>
+
+                    </div>
+                </div>
             </div>
         </section>
     )
