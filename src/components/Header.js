2e5d5766929e24cf1fa32fb1aa55b0f5099a101d
almeidagds <almeidag2001@gmail.com>
2021-09-01 14:53:33 -0300

    Commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,16 @@ import './Header.css'
 export default () => {
     return (
         <header>
-            ...
+            <div className="header--logo">
+                <a href="#">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" alt="Netflix logo"/>
+                </a>
+            </div>
+            <div className="header--user">
+                <a href="#">
+                    <img src="https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png" alt="user avatar"/>
+                </a>
+            </div>
         </header>
     )
 }
