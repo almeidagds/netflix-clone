aa334e358bf0269ad2f1a5388b6d695cbb494309
almeidagds <almeidag2001@gmail.com>
2021-09-01 14:05:26 -0300

    Commit

--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -12,9 +12,9 @@ export default ({item}) => {
                     <div className="featured--info">
                         <div className="featured--points">{item.vote_average} pontos</div>
                         <div className="featured--year">2099</div>
-                        <div className="featured--seasons">{}</div>
-
+                        <div className="featured--seasons">{item.number_of_seasons} temporada{item.number_of_seasons !== 1 ? "s":""}</div>
                     </div>
+                    <div className="featured--description">{item.overview}</div>
                 </div>
             </div>
         </section>
