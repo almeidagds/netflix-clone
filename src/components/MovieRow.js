44d5c59b6764699b758af6e53d9133d632ed0c72
almeidagds <almeidag2001@gmail.com>
2021-09-04 20:02:56 -0300

    Commit

--- src/components/MovieRow.js
+++ src/components/MovieRow.js
@@ -7,11 +7,20 @@ export default ({title, items}) => {
     const [scrollX, setScrollX] = useState(0)
 
     const handleLeftArrow = () => {
-
+        let x = scrollX + Math.round(window.innerWidth) / 2;
+        if (x > 0) {
+            x = 0
+        }
+        setScrollX(x)
     }
 
     const handleRightArrow = () => {
-
+        let x = scrollX - Math.round(window.innerWidth) / 2;
+        let listW = items.results.length*150
+        if (x < Math.round(window.innerWidth) - listW) {
+            x = Math.round(window.innerWidth) - listW - 60
+        }
+        setScrollX(x)
     }
 
     return (
