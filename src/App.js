df7f524281c556644556571bbe72425bdf7d1e55
almeidagds <almeidag2001@gmail.com>
2021-09-06 13:45:23 -0300

    Commit

--- src/App.js
+++ src/App.js
@@ -59,6 +59,12 @@ export default () => {
           <p>Direitos de imagem para &copy; Netflix</p>
           <p>Dados coletados do site themoviedb.org</p>
       </footer>
+
+      {movieList.length <= 0 &&
+        <div className="loading">
+          <img src="https://media.filmelier.com/noticias/br/2020/03/Netflix_LoadTime.gif" alt="carregando" />
+        </div>
+      }
     </div>
   )
 }
